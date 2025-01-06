61051fadf9d7f38c8694ffd60d78e1fa6b156dbd
Jack <97072047+jaedgely@users.noreply.github.com>
2025-01-06 12:48:05 -0500

    Update XilinxADC.cpp

--- Vitis/src/XilinxADC.cpp
+++ Vitis/src/XilinxADC.cpp
@@ -1,1 +1,28 @@
+/*
+ * XADC.cpp
+ *
+ *  Created on: Jan 5, 2025
+ *      Author: Jack Edgely
+ */
+
+#include "XilinxADC.h"
+
+XADC::XADC()
+{
+	// Set up the xilinx adc here
+}
+
+float XADC::ReadVoltage(ADCChannel channel)
+{
+	return 3.3 * (this->ReadBits(channel) / 4095);
+}
+
+uint16_t XADC::ReadBits(ADCChannel channel)
+{
+	return 0xAA;
+}
+XADC::~XADC()
+{
+	// TODO Auto-generated destructor stub
+}
 
