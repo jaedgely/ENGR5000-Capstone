72b696d99e66534ef837f83f5fa3fecdbd67226c
Jack <97072047+jaedgely@users.noreply.github.com>
2025-01-08 19:03:27 -0500

    Update SPIController_tb.sv
    Validates SPI through a loopback procedure

--- Vivado/TB/SPIController_tb.sv
+++ Vivado/TB/SPIController_tb.sv
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Wentworth Institute of Technology
+// Engineer: Jack Edgely
 // 
-// Create Date: 01/07/2025 09:01:01 AM
+// Create Date: 12/29/2024 05:10:17 PM
 // Design Name: 
 // Module Name: SPIController_tb
 // Project Name: 
@@ -45,14 +45,14 @@ module SPIController_tb();
         .CPHA(clockPha),
         .DATA_LEN(dataLength),
         .CS_i(chipSelect),
-        .D_TX(dataTx),
+        .TxBuffer(dataTx),
         .CIPO(CIPO),
         .COPI(COPI),
         .PCLK(PCLK),
         .BUSY(BUSY),
         .STARTING(STARTING),
         .CS_gpio(CS_gpio),
-        .Q_RX(dataRx)
+        .RxBuffer(dataRx)
     );
     
     assign CIPO = COPI;
@@ -70,6 +70,7 @@ module SPIController_tb();
         // 1 start cycle + 8 data + 1 end + 2 to track idle
         repeat(12) begin
             CycleClock();
+            start = 0;
         end
     endtask
     
@@ -82,17 +83,21 @@ module SPIController_tb();
         // 1 start cycle + 8 data + 1 end + 2 to track idle
         repeat(20) begin
             CycleClock();
+            start = 0;
         end
     endtask
     
     task Write24(logic[23:0] data, logic[7:0] chip);
-        dataTx[31:8] = data;
+        dataTx[3] = data[23:16];
+        dataTx[2] = data[15:8];
+        dataTx[1] = data[7:0];
         dataLength = 2'b10;
         chipSelect = chip;
         start = 1;
         // 1 start cycle + 8 data + 1 end + 2 to track idle
         repeat(30) begin
             CycleClock();
+            start = 0;
         end
     endtask
     
@@ -104,14 +109,18 @@ module SPIController_tb();
         // 1 start cycle + 8 data + 1 end + 2 to track idle
         repeat(38) begin
             CycleClock();
+            start = 0;
         end
     endtask
     
-    task ValidateCS_GPIO(logic [7:0] chip);
-    
+    task ValidateCS_GPIO(logic[7:0] chip);
+        assert(CS_gpio == chip)
+            $display("Chip select GPIO pins match expected output. Measured %d, expected %d", CS_gpio, chipSelect);
+        else
+            $display("!!!!!!!!!!!!!!!!!!/n Chip select GPIO pins do not match expected output. Measured %d, expected %d !!!!!!!!!!!!!!!!!!", CS_gpio, chipSelect);
     endtask
     
-    task ValidateDataRx(logic [1:0] register, logic[7:0] data);
+    task ValidateDataRx(logic[1:0] register, logic[7:0] data);
         assert (dataRx[register] == data)
             $display("Received data matches expected data. Measured 0x%h, expected 0x%h", dataRx[register], data);
         else
@@ -125,7 +134,44 @@ module SPIController_tb();
             $display("Pass - clock idles %d when CPOL is %d", PCLK, cpolLevel);
         else
             $display("Fail - clock idls %d when CPOL is %d", PCLK, cpolLevel);
-    endtask         
+    endtask  
+    
+    task Test1Byte();
+        logic[7:0] data = $urandom_range((2**8) - 1, 0);
+        int chipSelect = $urandom_range(255, 0);
+        Write8(data, chipSelect);        
+        ValidateDataRx(0, data);
+    endtask
+    
+    task Test2Byte();
+        logic[15:0] data = $urandom_range((2**16) - 1, 0);
+        int chipSelect = $urandom_range(255, 0);
+        Write16(data, chipSelect);        
+        ValidateDataRx(1, data[15:8]);
+        ValidateDataRx(0, data[7:0]);
+    endtask  
+    
+    task Test3Byte();
+        logic[23:0] data = $urandom_range((2**24) - 1, 0);
+        int chipSelect = $urandom_range(255, 0);
+        Write24(data, chipSelect);      
+        ValidateDataRx(2, data[23:16]);  
+        ValidateDataRx(1, data[15:8]);
+        ValidateDataRx(0, data[7:0]); 
+    endtask  
+    
+    task Test4Byte();
+        logic[15:0] data0 = $urandom_range((2**16) - 1, 0);
+        logic[15:0] data1 = $urandom_range((2**16) - 1, 0);
+        logic[31:0] dataConcat = {data1, data0};
+        int chipSelect = $urandom_range(255, 0);
+        Write32(dataConcat, chipSelect);     
+        ValidateDataRx(3, dataConcat[31:24]);   
+        ValidateDataRx(2, dataConcat[23:16]);  
+        ValidateDataRx(1, dataConcat[15:8]);
+        ValidateDataRx(0, dataConcat[7:0]);   
+    endtask  
+        
     
     initial begin
         clock = 0;
@@ -141,18 +187,25 @@ module SPIController_tb();
         CycleClock();
         ValidateCPOL(0);
         ValidateCPOL(1);
-        CycleClock();
-        CycleClock();
-        Write8('hAC, 7'b111_1110);
-        ValidateDataRx(0, 'hAC);
-        clockPol = 0;
-        clockPha = 0; #1;
-        Write16('hDEAD, 7'b111_0111);
-        clockPol = 1; #1;
-        Write24('hCE11FF, 7'b101_1111);
-        clockPha = 1; #1;
-        Write32('h89AB_CDEF, 7'b111_1011);
+
+        
+        for (int i = 0; i < 4; i++) begin
+            // Update SPI Mode
+            {clockPol, clockPha} = i; #1;
+            $display("TESTING SPI MODE %d", i);
+            
+            for (int j = 0; j < 4; j++) begin
+            $display("TESTING %d BYTE TRANSMISSION", j + 1);
+                repeat(3) begin
+                    case(j)
+                        0: Test1Byte();
+                        1: Test2Byte();
+                        2: Test3Byte();
+                        3: Test4Byte();
+                    endcase
+                end
+            end
+        end        
     end
    
 endmodule
-
