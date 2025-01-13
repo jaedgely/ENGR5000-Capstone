cadeac1f218ca34c1ae8d794e8c06e085eeff64c
Jack <97072047+jaedgely@users.noreply.github.com>
2025-01-13 18:21:34 -0500

    Update I2CControllerAXI_READY.sv

--- Vivado/HDL/I2CControllerAXI_READY.sv
+++ Vivado/HDL/I2CControllerAXI_READY.sv
@@ -19,59 +19,68 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
     
-module I2CControllerAXI_READY(
-    input clk,                          // Make 4x faster than desired I2C_SCL frequency
+module I2CControllerAXI_READY
+(
+    input clk,      
     input [31:0] AXI_IN,
+    input [15:0] TX_BUFFER,
     output [31:0] AXI_OUT,
-    output NACK,                        // Also part of AXI_OUT. This is for an LED
-    inout I2C_SCL_t,                    // Clock line to peripheral device
-    inout I2C_SDA_t);                   // Tristate data line (Pushpull, Xilinx does not support open drain)
+    output NACK,    
+    inout I2C_SCL_t,
+    inout I2C_SDA_t
+);  
     
     wire rst_n;
+    wire FORCE_CLK;
     wire start;
     wire OP_CODE;
-    wire send2bytes;
+    wire DATA_LEN;
+    wire SEND_REG_ADDR;
+    wire REG_ADDR_LEN;
     wire [1:0] FRQ_SEL;
-    wire FORCE_CLK;
     wire [6:0] PERIPH_ADDR;
+    wire [15:0] REG_ADDR;
     wire [15:0] D_TX;
-    wire [15:0] Q_RX;
+    wire [15:0] D_RX;
     wire BUSY;
     wire NACK;
-    
-    wire [1:0] reserved1;
-    wire [31:18] reserved2; 
     
     assign rst_n = AXI_IN[0];
     assign start = AXI_IN[1];
     assign OP_CODE = AXI_IN[2];
-    assign send2bytes = AXI_IN[3];
-    assign FRQ_SEL = AXI_IN[5:4];
-    assign FORCE_CLK = AXI_IN[6];
-    assign reserved0 = AXI_IN[7];
-    assign PERIPH_ADDR = AXI_IN[14:8];
-    assign reserved1 = AXI_IN[15];
-    assign D_TX = AXI_IN[31:16];
+    assign DATA_LEN = AXI_IN[3];
+    assign SEND_REG_ADDR = AXI_IN[4];
+    assign REG_ADDR_LEN = AXI_IN[5];
+    assign FRQ_SEL = AXI_IN[7:6];
+    assign FORCE_CLK = AXI_IN[8];
+    assign PERIPH_ADDR = AXI_IN[15:9];
+    assign REG_ADDR = AXI_IN[31:16];
+    assign D_TX = TX_BUFFER;
     
-    assign AXI_OUT[15:0] = Q_RX;
+    assign AXI_OUT[15:0] = D_RX;
     assign AXI_OUT[16] = BUSY;
     assign AXI_OUT[17] = NACK;
     assign AXI_OUT[31:18] = {'hBAD, 'b00};
     
-    I2CControllerTop I2C_CONTROLLER(
+    I2CControllerTop #(2)I2C_CONTROLLER
+    (
         .clk(clk),
         .rst_n(rst_n),
         .start(start),
         .OP_CODE(OP_CODE),
         .FORCE_CLK(FORCE_CLK),
-        .send2bytes(send2bytes),
+        .DATA_LEN(DATA_LEN),
+        .SEND_REG_ADDR(SEND_REG_ADDR),
+        .REG_ADDR_LEN(REG_ADDR_LEN),
         .FRQ_SEL(FRQ_SEL),
         .PERIPH_ADDR(PERIPH_ADDR),
+        .REG_ADDR(REG_ADDR),
         .D_TX(D_TX),
-        .Q_RX(Q_RX),
+        .D_RX(D_RX),
         .BUSY(BUSY),
         .NACK(NACK),
         .I2C_SCL_t(I2C_SCL_t),
-        .I2C_SDA_t(I2C_SDA_t));
+        .I2C_SDA_t(I2C_SDA_t)
+    );
         
 endmodule
