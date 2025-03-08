4c60e1686ff8d171b1deddf9fbb086ef5c8ebac4
Jack <97072047+jaedgely@users.noreply.github.com>
2025-03-08 12:09:39 -0500

    Update I2CController_tb.sv

--- Vivado/TB/I2CController_tb.sv
+++ Vivado/TB/I2CController_tb.sv
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: Wentworth Institute of Technology
-// Engineer: Jack Edgely
+// Company: 
+// Engineer: 
 // 
-// Create Date: 01/01/2025 03:51:05 PM
+// Create Date: 01/16/2025 01:41:12 PM
 // Design Name: 
 // Module Name: I2CController_tb
 // Project Name: 
@@ -21,184 +21,98 @@
 
 
 module I2CController_tb();
+
     logic clock;
     logic reset;
     logic start;
+    logic stop;
     logic opCode;
+    logic sendTenBitAddr;
     logic forceClock;
-    logic [1:0] bytesToSend;
-    logic [6:0] address;
+    logic [1:0] freqSel;
+    logic [6:0] periphAddr;
+    logic [15:0] regAddr;
     logic [15:0] dataTx;
-    logic [15:0] dataRx;   // Try to load 0xCE11
-    logic busyBit;
-    logic nackBit;
+    logic [15:0] dataRx;
+    logic busy;
+    logic loading;
+    logic starting;
+    logic nack;
     
-    wire SDA;
-    wire SCL;
+    wire I2C_SCL;
+    wire I2C_SDA;
     
-    pullup(SDA);
-    pullup(SCL);
-
-    I2CController #(2)DUT
+    I2CController #(100_000_000) DUT
     (
         .clk(clock),
         .rst_n(reset),
         .start(start),
-        .OP_CODE(opCode),
-        .FORCE_CLK(forceClock),
-        .bytesToSend(bytesToSend),
-        .PERIPH_ADDR(address),
-        .D_TX(dataTx),
-        .Q_RX(dataRx),
-        .BUSY(busyBit),
-        .NACK(nackBit),
-        .I2C_SCL_t(SCL),
-        .I2C_SDA_t(SDA)
+        .stop(stop),
+        .rwbit(opCode),  
+        .tenBitAddr(sendTenBitAddr),
+        .forceClock(forceClock),
+        .prescale(40),
+        .periphAddr(periphAddr),
+        .txBuffer(dataTx),
+        .rxBuffer(dataRx),
+        .busy(busy),
+        .loading(loading),
+        .starting(starting),
+        .nack(nack),
+        .I2C_SCL_i(I2C_SCL),
+        .I2C_SCL_o(I2C_SCL),
+        .I2C_SDA_i(1'b0),
+        .I2C_SDA_o(I2C_SDA)
     );
-    
-    // 100KHz - slowest i2c speed
-    task CycleClocks(); 
-        clock = ~clock; #1250;
-        clock = ~clock; #1250;
+   
+    task CycleClock();
+        clock = ~clock; #5;
+        clock = ~clock; #5;
     endtask
-    
-    task WriteData(logic [6:0] pAddr, logic [15:0] data, logic[1:0] numBytes, logic addrAck, logic dataAck0, logic dataAck1);
-        start = 0;
-        $display("Testing I2C Controller write functionality.");
-        $display("\t Targeting device 0x%h with data 0x%h.", pAddr, data);
-        $display("\t Will force the following: ADDR_ACK = %b, DATA_ACK_0 = %b, DATA_ACK_1 = %b", addrAck, dataAck0, dataAck1);
-                 
-        address = pAddr;
-        opCode = 0;
-        dataTx = data;
-        bytesToSend = numBytes;
-        
-        $display("\t Writing 0x%h to device 0x%h", data, pAddr);
-        
-        start = 1;
-        
-        for (int i = 0; i < 35; i++) begin
-            $display("Cycle %d", i);
-            CycleClocks();
-        end
-        
-        TestACK(0);
-        
-        // 8 data bits + 1 ack == 9
-        repeat(31) begin;
-            CycleClocks();
-        end
-        TestACK(0);
-        
-        repeat(31) begin;
-            CycleClocks();
-        end
-        TestACK(0);
-        
-        repeat(8) begin;
-            CycleClocks(); 
-        end
-        
-    endtask    
-    
-    task ReadData(logic[6:0] pAddr, logic[15:0] data, logic[1:0] numbytes);
-        start = 0;
-        $display("Testing I2C Controller read functionality.");
-        $display("\t Targeting device 0x%h with data 0x%h.", pAddr, data);
-                 
-        address = pAddr;
-        opCode = 1;
-        dataTx = data;
-        bytesToSend = numbytes;
-        
-        $display("\t Writing 0x%h to device 0x%h", data, pAddr);
-        
-        start = 1;
-        
-        // 1 start bit + 7 addr + 1 command = 9 bits
-        // 4 * 9 = 36
-        repeat(36) begin;
-            CycleClocks();
-        end
-        start = 0;
-        TestACK(0);
-        
-        // 8 data bits + 1 ack == 9
-        repeat(31) begin;
-            CycleClocks();
-        end
-        TestACK(0);
-        
-        repeat(31) begin;
-            CycleClocks();
-        end
-        TestACK(0);
-        
-        repeat(8) begin;
-            CycleClocks(); 
-        end
-    endtask;
-    
-    task TestACK(logic NACK);
-        clock = ~clock; #1240;
-        #10;
-        CheckSDATristate();
-        force SDA = NACK;      
-        CycleClocks();
-        //$display("\t Wrote %b to SDA line", NACK);
-        CycleClocks();
-        
-        assert(nackBit == NACK)
-            $display("\t Pass - NACK output matches expected value");
-        else
-            $display("\t Fail - NACK output does not match expected. Measured %d, expected %d", nackBit, NACK);
-
-        CycleClocks();
-        CycleClocks();
-        release SDA;
-    endtask;
-     
-    task CheckSDATristate();
-        assert(SDA === 1'b1)    // Due to pullup, will pulse 1
-            $display("\t Pass - I2C Controller succesfully tri-stated I2C_SDA line before next I2C_SCL pulse");
-        else
-            $display("\t Fail - I2C Controller is still driving I2C_SDA_t to %d", SDA);
-    endtask
-  
     
     initial begin
         clock = 0;
         reset = 0;
         start = 0;
+        stop = 0;
         opCode = 0;
         forceClock = 0;
-        bytesToSend = 1;
-        address = 'hFF;
-        dataTx = 'hFFFF;
-        CycleClocks();
-        CycleClocks();
-        CycleClocks();
-        CycleClocks();
-        reset = 1;
-        CycleClocks();
-        CycleClocks();
-        CycleClocks();  
-        CycleClocks();    
+        sendTenBitAddr = 0;  
+        freqSel = 0;
+        periphAddr = 0;
+        regAddr = 0;
+        dataTx = 0;
         
-        WriteData('hA5, 'h550F, 1, 0, 0, 0);
-        ReadData('h24, 'hABCD, 1);
-        reset = 0;
-        repeat(25) begin;
-            CycleClocks();
+        repeat(2) begin
+            CycleClock();
         end
-            
+        
         reset = 1;
-        repeat(25) begin;
-            CycleClocks();
+        periphAddr = 7'h13;
+        opCode = 0;
+        dataTx = 16'hDEAD;
+        regAddr = 16'hCE11;
+        
+        repeat(1000) begin
+            CycleClock();
         end
-        WriteData('hD3, 'hDEAD, 1, 0, 0, 0); 
-
-        WriteData('h35, 'hCC00, 0, 0, 0, 0);
+        
+        start = 1;
+        
+        repeat(4000) begin
+            CycleClock();
+        end
+        
+        start = 0;
+        repeat(4000) begin
+            CycleClock();
+        end
+        
+        stop = 1;
+        opCode = 1;
+        
+        repeat(80000) begin
+            CycleClock();
+        end
     end
-    
 endmodule
